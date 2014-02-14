657fae1524ea23fdc5fdda404ae19670e301f3ed
Simon Hordvik <simon.hordvik@gmail.com>
2014-02-14 03:24:48 -0800

    asd

--- app/controllers/loginCtrl.js
+++ app/controllers/loginCtrl.js
@@ -13,10 +13,7 @@ function loginCtrl($scope, $location){
     var timer;
     var counter = 0;
     $scope.accel = function() {
-        //navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
-        //var options = { frequency: 1000 };   //has to be between 40 ms and 1000 ms on iphone
-        timer = setInterval(function(){navigator.accelerometer.getCurrentAcceleration(onSuccess, onError)}, 1000);
-        //navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+        timer = setInterval(function(){navigator.accelerometer.getCurrentAcceleration(onSuccess, onError)}, 500);
     };
 
 
@@ -24,6 +21,7 @@ function loginCtrl($scope, $location){
     // onSuccess: Get a snapshot of the current acceleration
     //
     function onSuccess(acceleration) {
+        var wankString = $("#wankString").text()
         var element = $("#mongo");
         counter++;
         element.text('Acceleration X: ' + acceleration.x + '<br />' +
@@ -31,16 +29,16 @@ function loginCtrl($scope, $location){
             'Acceleration Z: ' + acceleration.z + '<br />' +
             'Timestamp: '      + acceleration.timestamp + '<br />' +
             counter);
-        if (counter > 10) {
+        if (acceleration.y > 20) {
+            $("#wankString").text(wankString+"-");
+        }
+        if (wankString.length > 40) {
             clearInterval(timer);
         }
     }
 
     function stopWatch() {
-        //if (watchID) {
-            navigator.accelerometer.clearWatch(watchID);
-          //  watchID = null;
-        //}
+        navigator.accelerometer.clearWatch(watchID);
     }
 
     // onError: Failed to get the acceleration
